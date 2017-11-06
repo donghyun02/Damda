cd0f0ea7b6a12accb0d4f1560ba334e160a5e39a
김동현 <almond@gimdonghyeon-ui-MacBook-Pro.local>
2017-11-06 21:23:23 +0900

    Initial project

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from accounts import views
+
+urlpatterns = [
+    url(r'^login/$', views.Login.as_view()),
+    url(r'^register/$', views.Register.as_view()),
+]
---
+++ accounts/views.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+# Create your views here.
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class Login(View):
+    def get(self, request):
+        return render(request, 'accounts/login.html')
+
+@method_decorator(csrf_exempt, name='dispatch')
+class Register(View):
+    def get(self, request):
+        return render(request, 'accounts/register.html')
