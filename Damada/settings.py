ae33595f5c4cc462a87ae1138c52927653b08a71
김동현 <almond@gimdonghyeon-ui-MacBook-Pro.local>
2017-11-12 23:49:28 +0900

    Add main page html

--- Damada/settings.py
+++ Damada/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'social_django',
     'accounts',
+    'capture',
 ]
 
 MIDDLEWARE = [
--- Damada/urls.py
+++ Damada/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('accounts.urls')),
+    url(r'^app/', include('capture.urls')),
     url('', include('social_django.urls', namespace='social')),
 ]
