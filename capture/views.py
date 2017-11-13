1908e25cc6fbb5e492ebddf10d8b3d7e549872d1
김동현 <almond@gimdonghyeon-ui-MacBook-Pro.local>
2017-11-13 23:08:20 +0900

    Change redirect url

--- capture/views.py
+++ capture/views.py
@@ -6,7 +6,6 @@ from django.shortcuts import render
 # Create your views here.
 from django.views import View
 
-
 class Index(View):
     def get(self, request):
         return render(request, 'capture/index.html')
