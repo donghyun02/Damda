bf316c61d26e398a1871d279a0ec7b0aab6d12b5
김동현 <almond@gimdonghyeon-ui-MacBook-Pro.local>
2017-11-15 00:20:14 +0900

    Make user settingt

--- capture/models.py
+++ capture/models.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+    owner = models.ForeignKey(User)
+
+class Bookmark(models.Model):
+    name = models.CharField(max_length=50)
+    url = models.TextField()
+    tags = models.ManyToManyField(Tag)
+
+class Folder(models.Model):
+    name = models.CharField(max_length=20)
+    owner = models.ForeignKey(User)
+    bookmarks = models.ManyToManyField(Bookmark)
