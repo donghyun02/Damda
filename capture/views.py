f34ef35f2d4b8b6ef440cb1addf175010dfbb830
김동현 <almond@gimdonghyeon-ui-MacBook-Pro.local>
2017-11-17 04:53:40 +0900

    Feature that can modify tag or folder in bookmark

--- capture/views.py
+++ capture/views.py
@@ -49,6 +49,19 @@ class FolderRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Folder.objects.all()
     serializer_class = FolderSerializer
 
+    def destroy(self, request, *args, **kwargs):
+        print(request.data.get('name'))
+        try:
+            name = request.data.get('name')
+            f = Folder.objects.get(name=name)
+            for i in f.bookmarks.all():
+                i.delete()
+        except:
+            pass
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 class TagListCreateView(generics.ListCreateAPIView):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
@@ -72,15 +85,32 @@ class BookmarkListCreateView(generics.ListCreateAPIView):
         folderName = request.POST.get('folderName')
         pk = serializer.data['id']
         bookmark = Bookmark.objects.get(pk=pk)
-        folder = Folder.objects.get(name=folderName)
+        folder = Folder.objects.get(name=folderName, owner__username=request.user)
         folder.bookmarks.add(bookmark)
         folder.save()
 
-        url = request.POST.get('url')
-        webpage = urlopen(url).read()
-        soup = BeautifulSoup(webpage, "html.parser")
-        imageURL = soup.find('meta', property='og:image')
-        imageURL = imageURL['content'] if url else "/static/capture/images/logo.png"
+        try:
+            try:
+                url = request.POST.get('url')
+                webpage = urlopen(url).read()
+                soup = BeautifulSoup(webpage, "html.parser")
+                imageURL = soup.find('meta', property='og:image')
+                imageURL = imageURL['content'] if url else "/static/capture/images/bookmark.png"
+            except:
+                try:
+                    url = 'http://' + request.POST.get('url')
+                    webpage = urlopen(url).read()
+                    soup = BeautifulSoup(webpage, "html.parser")
+                    imageURL = soup.find('meta', property='og:image')
+                    imageURL = imageURL['content'] if url else "/static/capture/images/bookmark.png"
+                except:
+                    url = 'https://' + request.POST.get('url')
+                    webpage = urlopen(url).read()
+                    soup = BeautifulSoup(webpage, "html.parser")
+                    imageURL = soup.find('meta', property='og:image')
+                    imageURL = imageURL['content'] if url else "/static/capture/images/bookmark.png"
+        except:
+            imageURL = "/static/capture/images/bookmark.png"
 
         bookmark.imageURL = imageURL
         bookmark.save()
@@ -91,6 +121,46 @@ class BookmarkListCreateView(generics.ListCreateAPIView):
 class BookmarkRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Bookmark.objects.all()
     serializer_class = BookmarkSerializer
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+
+        try:
+            folder = Folder.objects.get(name=instance.folderName, owner__username=request.user)
+            folder.bookmarks.remove(instance)
+            folder.save()
+        except:
+            pass
+
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        try:
+            folderName = request.data.get('folderName')
+            print(folderName)
+            folder = Folder.objects.get(owner__username=request.user, name=folderName)
+            folder.bookmarks.add(instance)
+            folder.save()
+
+        except:
+            pass
+
+        try:
+            tagName = request.data.get('tag')
+            tag = Tag.objects.get(owner__username=request.user, name=tagName)
+            instance.tags.add(tag)
+            instance.save()
+        except:
+            pass
+
+        return Response(serializer.data)
 
 @method_decorator(csrf_exempt, name='dispatch')
 class ChangeNameView(View):
