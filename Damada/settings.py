482a18214983b74a3dbb8ed2d8ada8fb2ce6b03f
dhkim2013 <dhkim20157@gmail.com>
2017-11-17 09:03:43 +0900

    Change request domain

--- Damada/settings.py
+++ Damada/settings.py
@@ -202,5 +202,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'http://35.166.12.115/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
