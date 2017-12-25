93cbf0a1a47ccad4a9cea85e4dabefc6be6ad0ed
김동현 <almond@gimdonghyeon-ui-MacBook-Pro.local>
2017-12-25 20:50:34 +0900

    Delete slash

--- capture/urls.py
+++ capture/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^folders/$', views.FolderListCreateView.as_view()),
     url(r'^folders/(?P<pk>[0-9]+)/$', views.FolderRetrieveUpdateDestroyView.as_view()),
     url(r'^tags/$', views.TagListCreateView.as_view()),
-    url(r'^tags/(?P<pk>[0-9]+)/$', views.TagRetrieveUpdateDestroyView.as_view()),
+    url(r'^tags/(?P<pk>[0-9]+)$', views.TagRetrieveUpdateDestroyView.as_view()),
     url(r'^bookmarks/$', views.BookmarkListCreateView.as_view()),
     url(r'^bookmarks/(?P<pk>[0-9]+)/$', views.BookmarkRetrieveUpdateDestroyView.as_view()),
     url(r'^username/$', views.ChangeNameView.as_view()),
