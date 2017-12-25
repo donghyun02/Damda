8a1a72d05f17b91c17cb8ae63d4e215326321cab
김동현 <almond@gimdonghyeon-ui-MacBook-Pro.local>
2017-12-25 15:39:04 +0900

    Add Note model

--- capture/admin.py
+++ capture/admin.py
@@ -4,8 +4,9 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
-from .models import Folder, Tag, Bookmark
+from .models import Folder, Tag, Bookmark, Note
 
 admin.site.register(Folder)
 admin.site.register(Tag)
 admin.site.register(Bookmark)
+admin.site.register(Note)
